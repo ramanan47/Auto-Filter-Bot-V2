858c3bff83d350b1a6107dc41f6a0d0d5a8184d5
anandhu9188774739 <84672154+anandhu9188774739@users.noreply.github.com>
2021-05-23 22:15:15 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,64 +1,18 @@
 class script(object):
 
+    START_MSG = """ Hey {}
 
-    START_MSG = 
+നീ ഏതാ മോനൂസെ എന്നെ [𝗖𝗜𝗡𝗘𝗠𝗔 𝗖𝗢𝗠𝗣𝗔𝗡𝗬](https://t.me/cinimacompany123)ഗ്രൂപ്പിലേക്ക് മാത്രമേ ഉപയോഗിക്കാൻ പറ്റൂ...
 
-"""Hey {}
-നീ ഏതാ മോനൂസെ എന്നെ [𝗖𝗜𝗡𝗘𝗠𝗔 𝗖𝗢𝗠𝗣𝗔𝗡𝗬]( https://t.me/cinimacompany123)ഗ്രൂപ്പിലേക്ക് മാത്രമേ ഉപയോഗിക്കാൻ പറ്റൂ...
 വെറുതെ സമയം കളയാൻ നിൽക്കണ്ട...വേഗം ഗ്രൂപ്പിലേക്ക് വിട്ടോ സിനിമ അവിടെ കിട്ടും...🤭"""
-HELP_MSG = """<b>How to use the bot??</b>
 
-<i>
-* Add bot to your group with admin rights.
-
-* Add bot to channels which you want to link with <b>all admin rights</b>!
-</i>
-
-
-<b>Bot Commands - Works in Group only</b>
-(You need to be a Auth User in order to use these commands)
-
-* <code>/add channelid</code>  -  Links channel to your group.
-or
-* <code>/add @channelusername</code> - Links channel to your group.
-
-<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
-
-
-* <code>/del channelid</code>  -  Delinks channel from group
-or
-* <code>/del @channelusername</code>  -  Delinks channel from group
-
-<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
-
-
-* <code>/delall</code>  -  Removes all connected channels and filters from group!
-
-<i>Note : Dont add command delete bots in group
-
-
-
-
-
-
-
-    ! Otherwise, delall command wont work</i>
-
-
-* <code>/filterstats</code>  -  Check connected channels and number of filters.
-
-
-
-No need add each filter again!
-Bot will automatically search for your files and give links to that!
-
-
-<b>© @TroJanzHEX</b>"""
-
+    HELP_MSG = "ഇപ്പോഴും ഇപ്പോഴും പറയാൻ വയ്യ എന്നെ കൊണ്ട് നിനക്ക് ഒരു help ഉം കിട്ടില്ല...😂"
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
+
     
-⭕️<b>Creater :</b> @TroJanzHEX
+
+⭕️<b>Creater :</b> @Cinema_company_owner
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
